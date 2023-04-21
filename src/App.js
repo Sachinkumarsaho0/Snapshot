7e6f54d31001928e32519545b3ea6c28b807e8e6
Sachinkumarsaho0 <124592113+Sachinkumarsaho0@users.noreply.github.com>
2023-04-21 11:17:31 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  const searchData = useRef(null);
+  const [searchText, setSearchText] = useState("mountain");
+  const [imageData, setImageData] = useState([]);
+  useEffect(() => {
+    const params = {
+      method: "flickr.photos.search",
+      api_key: "2257b5dbc4a56f38286779f397de39dd",
+      text: searchText,
+      sort: "",
+      per_page: 42,
+      license: "4",
+      extras: "owner_name , license",
+      format: "json",
+      nojsoncallback: 1,
+    };
+    const parameters = new URLSearchParams(params);
+    const url = `http://api.flickr.com/services/rest/?${parameters}`;
+    axios
+      .get(url)
+      .then((resp) => {
+        console.log(resp.data);
+        const arr = resp.data.photos.photo.map((imgData) => {
+          return fetchFlickerImageUrl(imgData, "q");
+        });
+        setImageData(arr);
+      })
+
+      .catch(() => {})
+      .finally(() => {});
+  }, [searchText]);
+
+  const fetchFlickerImageUrl = (photo, size) => {
+    let url = `http://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}`;
+    if (size) {
+      url += `_${size}`;
+    }
+    url += `.jpg`;
+    return url;
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <input
+        onChange={(e) => {
+          searchData.current = e.target.value;
+        }}
+      />
+      <button
+        onClick={() => {
+          setSearchText(searchData.current);
+        }}
+      >
+        Search
+      </button>
+      <section className="button">
+        <button
+          onClick={() => {
+            setSearchText("mountains");
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Mountains
+        </button>
+        <button
+          onClick={() => {
+            setSearchText("beaches");
+          }}
+        >
+          Beaches
+        </button>
+        <button
+          onClick={() => {
+            setSearchText("birds");
+          }}
+        >
+          Birds
+        </button>
+        <button
+          onClick={() => {
+            setSearchText("food");
+          }}
+        >
+          Food
+        </button>
+      </section>
+      <section className="image">
+        {imageData.map((imageurl, key) => {
+          return (
+            <article>
+              {" "}
+              <img src={imageurl} key={key} />;{" "}
+            </article>
+          );
+        })}
+      </section>
     </div>
   );
 }
